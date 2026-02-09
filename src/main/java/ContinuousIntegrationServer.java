4a1860d8107bd541a3245753e4d81d705fdfb2e2
benedictS. <eljaysam@gmail.com>
2026-02-09 22:29:30 +0100

    2 implement json parsing logic (#45)
    * feat: Implemented a payload parser
    
    * feat: Implemented parsing av ssh_url
    
    * refactor: All information regarding the payload can now eventually be stored in a PayloadParser object.
    
    * feat: Implemented parsing of branch, commitHash and repoName
    
    * fix: Handled cases where ref does not exist

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,42 +1,63 @@
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.ServletException;
 
 import java.io.IOException;
 
-import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 /**
- * Skeleton of a ContinuousIntegrationServer which acts as webhook
- * See the Jetty documentation for API documentation of those classes.
+ * Skeleton of a ContinuousIntegrationServer which acts as webhook See the Jetty
+ * documentation for API documentation of those classes.
  */
 public class ContinuousIntegrationServer extends AbstractHandler {
-  public void handle(String target,
-      Request baseRequest,
-      HttpServletRequest request,
-      HttpServletResponse response)
-      throws IOException, ServletException {
-    response.setContentType("text/html;charset=utf-8");
-    response.setStatus(HttpServletResponse.SC_OK);
-    baseRequest.setHandled(true);
 
-    System.out.println(target);
+    public void handle(String target,
+            Request baseRequest,
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws IOException, ServletException {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
 
-    // here you do all the continuous integration tasks
-    // for example
-    // 1st clone your repository
-    // 2nd compile the code
+        System.out.println(target);
 
-    response.getWriter().println("CI job done");
-  }
+        if ("POST".equalsIgnoreCase(request.getMethod())) {
+            try {
+                // Parse using our new utility
+                PayloadParser info = new PayloadParser();
+                info.parse(request);
+                
+                System.out.println(info.sshUrl);
+                System.out.println(info.repoName);
+                System.out.println(info.commitHash);
+                System.out.println(info.branch);
 
-  // used to start the CI server in command line
-  public static void main(String[] args) throws Exception {
-    Server server = new Server(8080);
-    server.setHandler(new ContinuousIntegrationServer());
-    server.start();
-    server.join();
-  }
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println("Payload received successfully");
+
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().println("Error processing JSON: " + e.getMessage());
+            }
+        }
+        // here you do all the continuous integration tasks
+        // for example
+        // 1st clone your repository
+        // 2nd compile the code
+
+        response.getWriter().println("CI job done");
+    }
+
+    // used to start the CI server in command line
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+        server.setHandler(new ContinuousIntegrationServer());
+        server.start();
+        server.join();
+    }
 }
