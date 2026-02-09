82ef63822fe835a96cc4427054f0d8d03f778214
Selino10101 <109251476+Selino10101@users.noreply.github.com>
2026-02-09 22:09:34 +0100

    feat: implemented a directory removal function and tests for it (#51)
    * refactor: changed BranchCloner to use an abstract class and deleted BranchPuller.java
    
    * feat: implemented a directory removal function and tests for it

---
+++ src/main/java/functions/DirectoryRemover.java
@@ -0,0 +1,34 @@
+package functions;
+
+import java.nio.file.Files;
+import static java.nio.file.Files.walk;
+import java.nio.file.Path;
+import java.util.Comparator;
+
+public class DirectoryRemover {
+    /**
+     * Function that removes a given directory
+     * @param dir Directory that should be removed
+     * @return Returns true on successful removal, returns false otherwise.
+     */
+    public boolean deleteDirectory(Path dir) {
+        if (dir == null || !Files.exists(dir)) {
+            return false;
+        }
+        try {
+            final boolean[] failed = {false};
+            walk(dir).sorted(Comparator.reverseOrder()).forEach(path -> {
+                try {
+                    Files.delete(path);
+                } catch (Exception e) {
+                    failed[0] = true;
+                    e.printStackTrace();
+                }
+            });
+            return !failed[0];
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
