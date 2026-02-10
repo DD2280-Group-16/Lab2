5d9368a8a62dfd165e5d0caa65455f99c46d6ed4
Selino10101 <MackanOlson@hotmail.com>
2026-02-10 09:21:31 +0100

    feat: implemented tests for the PayloadParser

--- src/test/java/PayloadParserTest.java
+++ src/test/java/PayloadParserTest.java
@@ -2,6 +2,7 @@ import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -54,5 +55,47 @@ public class PayloadParserTest {
         assertEquals(parser.repoName, "test/repo");
         assertEquals(parser.commitHash, "test123");
         assertEquals(parser.branch, "main");
+    }
+
+    /**
+     * Test that verifies that the parser throws IllegalArgumentException when the body is missing "ref"
+     * @throws Exception
+     */
+    @Test
+    void testThatVerifiesThatTheParserThrowsExceptionOnNoRef() throws Exception {
+        String body =
+            "{"
+            + "  \"after\": \"test123\","
+            + "  \"repository\": {"
+            + "    \"ssh_url\": \"git@github.com:test/repo.git\","
+            + "    \"full_name\": \"test/repo\""
+            + "  }"
+            + "}";
+
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(body.getBytes(StandardCharsets.UTF_8));
+        ServletInputStream servletInputStream = mock(ServletInputStream.class);
+
+        /*
+            JSONTokener calls InputStreamReader which uses read(byte[] buffer, int off, int len)
+            Regular mocking resulted in zero bytes read.
+        */
+        when(servletInputStream.read(any(byte[].class), anyInt(), anyInt())).thenAnswer(invocation -> {
+            byte[] buffer = invocation.getArgument(0);
+            int off = invocation.getArgument(1);
+            int len = invocation.getArgument(2);
+            return byteStream.read(buffer, off, len);
+        });
+
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        when(request.getInputStream()).thenReturn(servletInputStream);
+
+        PayloadParser parser = new PayloadParser();
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> parser.parse(request)
+        );
+
+        assert(exception.getMessage().equals("Missing required field: ref"));
     }
 }
