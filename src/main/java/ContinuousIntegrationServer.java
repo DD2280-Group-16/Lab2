e9861a705e4ef944849613d7438062796f368078
Elin Andersson Lundell <elin.s.m.andersson@gmail.com>
2026-02-12 10:07:13 +0100

    Fix server start method call syntax

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -163,7 +163,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         server.setHandler(new ContinuousIntegrationServer());
-        server.start();
+        server.start
         server.join();
     }
 }
